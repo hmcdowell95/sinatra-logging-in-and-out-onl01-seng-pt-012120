c50d79192b2e6bf24adf309897dd3315389d9e96
Harley McDowell <hmcdowell95@gmail.com>
2020-04-28 19:33:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,10 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find(session[:user_id])
+    if @user.id == session[:user_id]
     erb :account
+    end
+    erb :error
   end
 
   get '/logout' do
