59f01d9766fd29ed461391d23002429c8a222779
Harley McDowell <hmcdowell95@gmail.com>
2020-04-28 20:04:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    binding.pry
     if @user && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect '/account'
